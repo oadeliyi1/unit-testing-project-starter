8f6989e78578d6c7911daafaa9cc1ee519951203
oadeliyi1 <oriyomiadeliyi@gmail.com>
2021-04-15 16:37:37 -0400

    Working on Phase 1

--- problems/phase-1.js
+++ problems/phase-1.js
@@ -3,7 +3,7 @@
 */
 function isFive(num) {
  if (num === 5) {
-   return 5;
+   return true;
  }
 }
 
@@ -25,13 +25,13 @@ function isOdd(number) {
 function myRange(min, max, step=1) {
   const array = []
   for (let i = min; i <= max; i++) {
-    array.push(i);  
+    array.push(i);
   }
   return array
 }
 /*4. Fizz Buzz Define a function fizzBuzz(max) that takes a number and prints
    every number from 0 to max (not inclusive) that is divisible by either 3 or
-   5, but not both. 
+   5, but not both.
 */
 
 function fizzBuzz(max) {
